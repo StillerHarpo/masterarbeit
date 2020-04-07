c68f3dd7c9097f356e1eeea9c03334fd3ed2de3c
Florian Engel <florianengel39@gmail.com>
2020-04-07 11:04:52 +0200

    Use a ghc version which is compatible with hie

--- release.nix
+++ release.nix
@@ -1,4 +1,19 @@
 let
-  pkgs = import <nixos-unstable> { };
+  compiler = "ghc882";
+  config = {
+    packageOverrides = pkgs: rec {
+      haskell = pkgs.haskell // {
+        packages = pkgs.haskell.packages // {
+          "${compiler}" = pkgs.haskell.packages."${compiler}".override {
+            overrides = haskellPackagesNew: haskellPackagesOld: rec {
+              masterarbeit =
+                haskellPackagesNew.callPackage ./masterarbeit.nix { };
+            };
+          };
+        };
+      };
+    };
+  };
+  pkgs = import <nixos-unstable> { inherit config; };
 in
-  pkgs.haskellPackages.callPackage ./masterarbeit.nix { }
+  pkgs.haskell.packages."${compiler}".masterarbeit
