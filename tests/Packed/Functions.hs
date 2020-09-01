c203dafedaff9a941526da262ab79776b12eb0a2
Florian Engel <florianengel39@gmail.com>
2020-09-01 12:29:48 +0200

    Generalize id for packed

--- tests/Packed/Functions.hs
+++ tests/Packed/Functions.hs
@@ -12,32 +12,32 @@ import Lib
 import Packed.Definition
 import Packed.Examples
 
-idD :: Text
-idD = T.unlines
-  [ "id = rec<Unit> Packed to Packed<Unit> where"
-  , "       { Pack x = Pack<Unit> @ x }"
+idD :: Text -> Text
+idD ty = T.unlines
+  [ "id = rec<" <> ty <> "> Packed to Packed<" <> ty <> "> where"
+  , "       { Pack x = Pack<" <> ty <> "> @ x }"
   ]
 
-idExpr :: Expr
-idExpr = WithParameters [UnitType] $
+idExpr :: TypeExpr -> Expr
+idExpr ty = WithParameters [ty] $
   Rec { fromRec = packedDucA
-      , toRec = GlobalTypeVar "Packed" [UnitType]
-      , matches = [ WithParameters [UnitType] (Constructor packedDucA 0)
+      , toRec = GlobalTypeVar "Packed" [ty]
+      , matches = [ WithParameters [ty] (Constructor packedDucA 0)
                     :@: LocalExprVar 0 "x"]}
 
 idTests :: Spec
 idTests = do
-  it "Parses id" $
-    shouldParseWithDefs [packedD] idD
+  it "Parses id<Unit>" $
+    shouldParseWithDefs [packedD] (idD "Unit")
       [ ExprDef { name = "id"
-                , expr = idExpr
+                , expr = idExpr UnitType
                 , ty = Nothing}]
-  it "Type checks id to (Packed<Unit>) -> Packed<Unit>" $
-    shouldCheckWithDefs [packedD] idExpr
+  it "Type checks id<Unit> to (Packed<Unit>) -> Packed<Unit>" $
+    shouldCheckWithDefs [packedD] (idExpr UnitType)
       ([packedExpr UnitType], GlobalTypeVar "Packed" [UnitType])
   it "Type checks id on packed () to Packed<Unit>" $
-    shouldCheckWithDefs [packedD] (idExpr :@: packedEx1Expr)
+    shouldCheckWithDefs [packedD] (idExpr UnitType :@: packedEx1Expr)
       ([], GlobalTypeVar "Packed" [UnitType])
   it "Evaluates id on packed () to packed ()" $
-    shouldEvalWithDefs [packedD] (idExpr :@: packedEx1Expr)
+    shouldEvalWithDefs [packedD] (idExpr UnitType :@: packedEx1Expr)
       packedEx1ExprI
