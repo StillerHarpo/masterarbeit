7c05dd55251ab386489d4a06cf3a0294b4c4d1b7
Florian Engel <florianengel39@gmail.com>
2020-10-10 17:21:10 +0200

    Add build script for org

---
+++ doc/build.sh
@@ -0,0 +1,12 @@
+#! /usr/bin/env nix-shell
+#! nix-shell -i bash -p "emacsWithPackages (epkgs: [ epkgs.melpaPackages.org-ref ])" python37Packages.pygments texlive.combined.scheme-full which --pure
+emacs \
+   --batch \
+   --eval '(require '\''org-ref)' \
+   --eval '(setq org-latex-listings '\''minted)' \
+   --eval '(setq reftex-default-bibliography '\''("./references.bib"))' \
+   --eval '(setq org-ref-default-bibliography reftex-default-bibliography)' \
+   --eval '(setq org-latex-prefer-user-labels '\''t)' \
+   masterarbeit.org \
+   -f org-latex-export-to-latex
+latexmk -shell-escape -bibtex -f -pdfxe masterarbeit.tex
