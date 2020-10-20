d2e62960a5892cdbc157be3b9cd6e5083fb58574
Florian Engel <florianengel39@gmail.com>
2020-10-20 10:09:59 +0200

    Change code printing

--- doc/init.el
+++ doc/init.el
@@ -1,7 +1,7 @@
 (setq-default indent-tabs-mode nil)
 (require 'org-ref)
 (setq org-latex-listings 'minted)
-(setq org-latex-minted-options '(("linenos") ("fontsize" "\\tiny")))
+(setq org-latex-minted-options '(("fontsize" "\\scriptsize")))
 (setq org-latex-prefer-user-labels 't)
 (eval-after-load "ox-latex"
   '(add-to-list 'org-latex-classes
