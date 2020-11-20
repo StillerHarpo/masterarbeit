2f5e55eb552066c5454b3d0c46db6ca3dc22ba0a
Florian Engel <florianengel39@gmail.com>
2020-11-20 22:18:15 +0100

    Add slides

---
+++ doc/buildSlides.sh
@@ -0,0 +1,8 @@
+#! /usr/bin/env nix-shell
+#! nix-shell -i bash -p "emacsWithPackages (epkgs: [ epkgs.melpaPackages.org-ref ])" python37Packages.pygments texlive.combined.scheme-full which --pure
+emacs \
+    --batch \
+    --load "init.el" \
+    slides.org \
+    -f org-beamer-export-to-latex
+latexmk -shell-escape -bibtex -f -pdfxe slides.tex
